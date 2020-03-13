f6bde2a3bee8c2ff3b575bfc26731310006e5e05
kb2shy <chhaianpin@hotmail.com>
2020-03-13 13:28:24 -0700

    completed intro vid

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,32 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { select } from 'd3';
 
-const data = [25, 33, 29, 50, 48];
-
 const App = (props) => {
+  const [data, setData] = useState([25, 30, 45, 60, 20]);
   const svgRef = useRef();
 
   useEffect(() => {
     const svg = select(svgRef.current);
-    console.log(svg.selectAll('circle').data(data));
-  }, []);
+    svg
+    .selectAll('circle')
+    .data(data)
+    .join('circle')
+    .attr('r', value => value)
+    .attr('cx', value => value * 2)
+    .attr('cy', value => value * 2)
+    .attr('stroke', 'red');
+  }, [data]);
 
   return (
     <React.Fragment>
       <svg ref={svgRef} ></svg>
+      <br />
+      <button onClick={() => setData(data.map(value => value + 5))}>
+        Update data
+      </button>
+      <button onClick={() => setData(data.filter(value => value <= 35))}>
+        Filter data
+      </button>
     </React.Fragment>
   )
 }
