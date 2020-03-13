4fa7d86c4b305d5971a0d412b0d4348fc4ce47fd
kb2shy <chhaianpin@hotmail.com>
2020-03-13 14:25:27 -0700

    completed vid3 axes and scales

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { select, line, curveCardinal } from 'd3';
+import { select, line, curveCardinal, axisBottom, axisRight, scaleLinear } from 'd3';
 
 const App = (props) => {
   const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
@@ -7,26 +7,45 @@ const App = (props) => {
 
   useEffect(() => {
     const svg = select(svgRef.current);
+    const xScale = scaleLinear().domain([0, data.length - 1]).range([0, 300])
+    const yScale = scaleLinear().domain([0, 150]).range([150, 0]);
+    const xAxis = axisBottom(xScale).ticks(data.length).tickFormat(index => index + 1);
+    const yAxis = axisRight(yScale);
+
+    svg
+      .select(".y-axis")
+      .style("transform", "translateX(300px)")
+      .call(yAxis);
+
+    svg
+      .select(".x-axis")
+      .style("transform", "translateY(150px)")
+      .call(xAxis);
+
     const myLine = line()
-      .x((value, index) => index * 50)
-      .y(value => 150 - value)
+      .x((value, index) => xScale(index))
+      .y(yScale)
       .curve(curveCardinal);
-    // svg
-    // .selectAll('circle')
-    // .data(data)
-    // .join('circle')
-    // .attr('r', value => value)
-    // .attr('cx', value => value * 2)
-    // .attr('cy', value => value * 2)
-    // .attr('stroke', 'red');
-    svg.selectAll("path").data([data]).join("path").attr("d", value => myLine(value)).attr("fill", "none").attr("stroke", "blue")
+
+    svg
+      .selectAll(".line")
+      .data([data])
+      .join("path")
+      .attr("class", "line")
+      .attr("d", value => myLine(value))
+      .attr("fill", "none")
+      .attr("stroke", "blue")
   }, [data]);
 
   return (
     <React.Fragment>
-      <svg ref={svgRef} style={{ backgroundColor: "lightgrey"}}>
-        
+      <svg ref={svgRef} style={{ backgroundColor: "lightgrey", overflow: "visible"}}>
+        <g className="x-axis" />
+        <g className="y-axis" />
       </svg>
+      <br />
+      <br />
+      <br />
       <br />
       <button onClick={() => setData(data.map(value => value + 5))}>
         Update data
