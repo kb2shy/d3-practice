1201f3e8e91ebae4cf3c685c54e30da9ab462665
kb2shy <chhaianpin@hotmail.com>
2020-03-13 13:43:04 -0700

    completed vid2 with line graph

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,32 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { select } from 'd3';
+import { select, line, curveCardinal } from 'd3';
 
 const App = (props) => {
-  const [data, setData] = useState([25, 30, 45, 60, 20]);
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
   const svgRef = useRef();
 
   useEffect(() => {
     const svg = select(svgRef.current);
-    svg
-    .selectAll('circle')
-    .data(data)
-    .join('circle')
-    .attr('r', value => value)
-    .attr('cx', value => value * 2)
-    .attr('cy', value => value * 2)
-    .attr('stroke', 'red');
+    const myLine = line()
+      .x((value, index) => index * 50)
+      .y(value => 150 - value)
+      .curve(curveCardinal);
+    // svg
+    // .selectAll('circle')
+    // .data(data)
+    // .join('circle')
+    // .attr('r', value => value)
+    // .attr('cx', value => value * 2)
+    // .attr('cy', value => value * 2)
+    // .attr('stroke', 'red');
+    svg.selectAll("path").data([data]).join("path").attr("d", value => myLine(value)).attr("fill", "none").attr("stroke", "blue")
   }, [data]);
 
   return (
     <React.Fragment>
-      <svg ref={svgRef} ></svg>
+      <svg ref={svgRef} style={{ backgroundColor: "lightgrey"}}>
+        
+      </svg>
       <br />
       <button onClick={() => setData(data.map(value => value + 5))}>
         Update data
